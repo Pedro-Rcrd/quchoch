c0ff6c76bc50632b8dda3cc5331781252c6233c2
pedro <pedrorlsapalu@gmail.com>
2023-04-20 22:46:39 -0600

    Cambio 20/04/2023

---
+++ Controllers/FileController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace systemquchooch.Controllers
+{
+    public class FileController : Controller
+    {
+        public IActionResult MostrarImagen()
+        {
+            return File("~/img/comunidad.jpg", "image/jpg");
+        }
+    }
+}
---
+++ Controllers/HomeController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace systemquchooch.Controllers
+{
+    public class HomeController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
